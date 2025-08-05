52e20cbfe9eb6c2430f2e49b0d24b1487f08f2a5
nakajimanaoto2014 <homurann1996@gmail.com>
2025-08-06 02:23:45 +0900

    Add files via upload

---
+++ spring_kadai_todo2/src/test/java/com/example/springkadaitodo/SpringKadaiTodo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkadaitodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKadaiTodo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
